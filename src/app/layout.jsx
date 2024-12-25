c6dd949f2ed3804a7faa496169c7abe14b8a1369
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-12-25 13:23:19 +0530

    vercel analytics added
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,17 @@
-import { Plus_Jakarta_Sans } from 'next/font/google';
-import { Toaster } from 'react-hot-toast';
-import './globals.css';
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const plus = Plus_Jakarta_Sans({
-  display: 'swap',
-  subsets: ['latin'],
+  display: "swap",
+  subsets: ["latin"],
 });
 
 export const metadata = {
-  title: 'GitHub Profile Reviewer',
+  title: "GitHub Profile Reviewer",
   description:
-    'A reviewer, roaster, and friend for your GitHub profile and to evaluate your GitHub profile.',
+    "A reviewer, roaster, and friend for your GitHub profile and to evaluate your GitHub profile.",
 };
 
 export default function RootLayout({ children }) {
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`bg-black text-white font-sans antialiased`}>
         <Toaster />
+        <Analytics />
         {children}
       </body>
     </html>
